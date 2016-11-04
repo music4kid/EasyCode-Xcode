6c3914df285d5224a4e0574610da2e9a248aa380
gao feng <1197902291@qq.com>
2016-11-04 18:18:06 +0800

    poilish

---
+++ EasyCode/MappingEditor/CustomButton.h
@@ -0,0 +1,13 @@
+//
+//  CustomButton.h
+//  EasyCode
+//
+//  Created by gao feng on 2016/11/4.
+//  Copyright © 2016年 music4kid. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CustomButton : NSButton
+
+@end
---
+++ EasyCode/MappingEditor/CustomButton.m
@@ -0,0 +1,19 @@
+//
+//  CustomButton.m
+//  EasyCode
+//
+//  Created by gao feng on 2016/11/4.
+//  Copyright © 2016年 music4kid. All rights reserved.
+//
+
+#import "CustomButton.h"
+
+@implementation CustomButton
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
